84ce3df96f33830a340177636d7e24e4e1a09d62
emilystephaniebatista <emilystephaniebatista@univem.edu.br>
2020-10-25 14:38:41 -0300

    banco

---
+++ Petshop.Server/Migrations/20201025173319_funcionario.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Petshop.Server.Migrations
+{
+    public partial class funcionario : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
